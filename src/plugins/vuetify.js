a45417e393e97320592ad2e4aacfb48dace476bf
Daniele Tribuzio <56612247+ZioTrib@users.noreply.github.com>
2021-02-07 11:39:51 +0100

    theme color palette definition

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,32 +6,40 @@ import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify);
 
+
 export default new Vuetify({
-  /* theme: {
+  theme: {
     themes: {
       light: {
-        primary: "#5aff14",
-        secondary: "#424242",
-        accent: "#82B1FF",
-        error: "#FF5252",
-        info: "#2196F3",
-        success: "#4CAF50",
-        warning: "#FFC107",
-        lightblue: "#14c6FF",
-        yellow: "#FFCF00",
-        pink: "#FF1976",
-        orange: "#FF8657",
-        magenta: "#C33AFC",
-        darkblue: "#1E2D56",
-        gray: "#909090",
-        neutralgray: "#9BA6C1",
-        green: "#2ED47A",
-        red: "#FF5c4E",
-        darkblueshade: "#308DC2",
-        lightgray: "#BDBDBD",
-        lightpink: "#FFCFE3",
-        white: "#FFFFFF"
+        primario: "#4D4E9D",
+        secondario: "#BEC0E3",
+        terziario: "#F89915",
+        // error: "#FF5252",
+        // info: "#2196F3",
+        // success: "#4CAF50",
+        // warning: "#FFC107",
+        // lightblue: "#14c6FF",
+        // yellow: "#FFCF00",
+        // pink: "#FF1976",
+        // orange: "#FF8657",
+        // magenta: "#C33AFC",
+        // darkblue: "#1E2D56",
+        // gray: "#909090",
+        // neutralgray: "#9BA6C1",
+        // green: "#2ED47A",
+        // red: "#FF5c4E",
+        // darkblueshade: "#308DC2",
+        // lightgray: "#BDBDBD",
+        // lightpink: "#FFCFE3",
+        // white: "#FFFFFF"
       }
     }
-  } */
+  },
+
+  palette: {
+    "Liberty":"4D4E9D",
+    "Periwinkle Crayola":"BEC0E3",
+    "Yellow Orange Color Wheel":"F89915",
+    "White":"FFFFFF"
+  }
 });
